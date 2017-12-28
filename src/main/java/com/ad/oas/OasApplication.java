5bea520d38a5791f6297c531bb98cf870c4f349f
lma25 <lma25@hawk.iit.edu>
2017-12-27 20:15:54 -0800

    Initial commit

---
+++ src/main/java/com/ad/oas/OasApplication.java
@@ -0,0 +1,12 @@
+package com.ad.oas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasApplication.class, args);
+	}
+}
